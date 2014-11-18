c0c36097a5f3d1a80bfc011f9d957914c4fb6a54
just-boris <just-boris@hotmail.com>
2014-11-19 01:07:52 +0300

    :fire: set remote tests only in firefox chrome cannot close properly

--- wct.conf.js
+++ wct.conf.js
@@ -1,4 +1,4 @@
 module.exports = {
     verbose: true,
-    browsers:    process.argv.indexOf('--remote') === -1 ? ['firefox'] : ['linux/firefox', 'linux/chrome@38']
+    browsers:    process.argv.indexOf('--remote') === -1 ? ['firefox'] : ['linux/firefox']
 };
